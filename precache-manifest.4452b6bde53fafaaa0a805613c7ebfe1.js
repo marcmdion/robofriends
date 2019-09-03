c2f81f1edfcc01be67245cbce084d78d2f185008
Marc M. Dion <mcmdion@yahoo.com>
2019-09-03 22:33:35 +1200

    Updates

---
+++ precache-manifest.4452b6bde53fafaaa0a805613c7ebfe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946e48f437a1a134c3aacd6860d9661f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "be0d1c0f59fa26a16269",
+    "url": "/robofriends/static/css/main.640b9d07.chunk.css"
+  },
+  {
+    "revision": "a4c405cc663d541fad1d",
+    "url": "/robofriends/static/js/2.52355532.chunk.js"
+  },
+  {
+    "revision": "be0d1c0f59fa26a16269",
+    "url": "/robofriends/static/js/main.315867bc.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
